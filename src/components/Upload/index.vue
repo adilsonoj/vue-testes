d8f7d6afe4a2b720293b1693b180c1e812da1b31
adilsonoj <adilsonoj@yahoo.com.br>
2020-12-06 20:22:36 -0300

    adiciona componente upload

---
+++ src/components/Upload/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <div v-if="currentFile">
+      <v-row no-gutters justify="center" align="center">
+        <v-col cols="12">
+          <v-progress-linear v-model="progress" color="light-blue" reactive>
+          </v-progress-linear>
+        </v-col>
+      </v-row>
+    </div>
+
+    <v-row no-gutters justify="center" align="center">
+      <v-col cols="8">
+        <v-file-input
+          show-size
+          label="Arquivo"
+          @change="selectFile"
+        ></v-file-input>
+      </v-col>
+
+      <v-col cols="4" class="pl-2">
+        <v-btn color="primary" dark small class="mr-5" @click="upload">
+          Upload
+          <v-icon right dark>mdi-cloud-upload</v-icon>
+        </v-btn>
+        <v-btn color="primary" fab dark small @click="upload">
+          <v-icon dark>mdi-cloud-upload</v-icon>
+        </v-btn>
+      </v-col>
+    </v-row>
+
+    <v-alert v-if="message" border="left" color="blue-grey" dark>
+      {{ message }}
+    </v-alert>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "upload-files",
+  data() {
+    return {
+      currentFile: "",
+      progress: "",
+      message: "",
+      fileInfos: [],
+    };
+  },
+  methods: {
+    selectFile(file) {
+      this.progress = 0;
+      this.currentFile = file;
+    },
+    upload() {
+      this.progress = 100;
+      console.log(this.currentFile);
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ src/pages/upload/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <Upload />
+</template>
+
+<script>
+import Upload from "../../components/Upload";
+export default {
+  name: "upload-page",
+  components: {
+    Upload,
+  },
+};
+</script>
+
+<style></style>
