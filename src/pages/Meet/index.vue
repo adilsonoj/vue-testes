80625d3765bea11d56271067aa55da9d6c82e398
Adilson Oliveira <adilsonoj@yahoo.com.br>
2020-12-10 21:06:38 -0300

    add option

---
+++ src/pages/Meet/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <JitsiMeet ref="jitsiRef" domain="meet.jit.si" :options="options" />
+</template>
+
+<script>
+import JitsiMeet from "../../components/JitsiMeet";
+export default {
+  components: {
+    JitsiMeet,
+  },
+  computed: {
+    name() {
+      return "user" + Math.floor(Math.random() * 100);
+    },
+    options() {
+      return {
+        roomName: "a456f4sd54fs1d3f1sd34gv5sd",
+        noSSL: false,
+        userInfo: {
+          email: this.name + "@email.com",
+          displayName: this.name,
+        },
+        configOverwrite: {
+          prejoinPageEnabled: false,
+          enableNoisyMicDetection: false,
+          enableWelcomePage: false,
+          disableDeepLinking: true,
+        },
+        interfaceConfigOverwrite: {
+          SHOW_JITSI_WATERMARK: false,
+          SHOW_WATERMARK_FOR_GUESTS: false,
+          SHOW_CHROME_EXTENSION_BANNER: false,
+          SHOW_PROMOTIONAL_CLOSE_PAGE: false,
+          HIDE_INVITE_MORE_HEADER: true,
+          PROVIDER_NAME: true,
+          MOBILE_APP_PROMO: false,
+          VIDEO_LAYOUT_FIT: "both",
+        },
+        onload: this.onIFrameLoad,
+      };
+    },
+  },
+  methods: {
+    onIFrameLoad() {
+      console.log("open meet");
+
+      this.$refs.jitsiRef.addEventListener("videoConferenceLeft", () =>
+        console.log("fechou a conexao")
+      );
+    },
+  },
+};
+</script>
