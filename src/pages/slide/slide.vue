59c33b5b172817f16e255079b9d62794326b3eeb
Adilson Oliveira <adilsonoj@yahoo.com.br>
2021-03-07 11:07:07 -0300

    adiciona imagens de tutorial gymbrain

--- src/pages/slide/slide.vue
+++ src/pages/slide/slide.vue
@@ -1,17 +1,10 @@
 <template>
-  <v-card
-    elevation="24"
-    max-width="360"
-    max-height="600"
-    class="mx-auto"
-  >
+  <v-card elevation="24" width="360" max-height="600" class="mx-auto">
     <v-carousel
       :continuous="false"
       :cycle="false"
       :show-arrows="true"
       hide-delimiter-background
-      max-height="600"
-      
     >
       <v-carousel-item
         v-for="(item, i) in slides"
@@ -20,35 +13,33 @@
         transition="fade-transition"
         :src="item.src"
       >
-       
       </v-carousel-item>
     </v-carousel>
-    
   </v-card>
 </template>
 <script>
-  export default {
-    data () {
-      return {
-        slides: [
-          {
-              src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg"
-          },
-           {
-              src: require("@/assets/slide/2.jpg")
-          },
-           {
-              src: require("@/assets/slide/3.jpg")
-          },
-           {
-              src: require("@/assets/slide/4.jpg")
-          },
-           {
-              src: require("@/assets/slide/5.jpg")
-          },
-        ],
-      }
-    },
-  }
+export default {
+  data() {
+    return {
+      slides: [
+        {
+          src: require("@/assets/tutorial/1.jpeg"),
+        },
+        {
+          src: require("@/assets/tutorial/2.jpeg"),
+        },
+        {
+          src: require("@/assets/tutorial/3.jpeg"),
+        },
+        {
+          src: require("@/assets/tutorial/4.jpeg"),
+        },
+        {
+          src: require("@/assets/tutorial/5.jpeg"),
+        },
+      ],
+    };
+  },
+};
 </script>
 
